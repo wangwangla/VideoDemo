691cccf018e24138c5d11fe04b548986b812042f
wangwangla <2818815189@qq.com>
2021-05-22 16:06:48 +0800

    欢迎页到跳转页

---
+++ app/src/main/java/com/kangwang/video/ui/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kangwang.video.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kangwang.video.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
---
+++ app/src/main/java/com/kangwang/video/ui/activity/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.kangwang.video.ui.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import com.kangwang.video.R;
+
+/**
+ * 显示logo
+ *  初始化数据
+ *  展示广告
+ */
+public class SplashActivity extends BaseActivity{
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+        //延迟跳转
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
