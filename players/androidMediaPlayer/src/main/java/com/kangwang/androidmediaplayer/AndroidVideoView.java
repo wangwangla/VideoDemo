d5ed84f6ae61e6d1f5317818efb81c66db806dfe
wangwangla <2818815189@qq.com>
2021-06-03 07:54:21 +0800

    自定义播放器

--- players/androidMediaPlayer/src/main/java/com/kangwang/androidmediaplayer/AndroidVideoView.java
+++ players/androidMediaPlayer/src/main/java/com/kangwang/androidmediaplayer/AndroidVideoView.java
@@ -1,33 +1,165 @@
 package com.kangwang.androidmediaplayer;
 
+import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.media.AudioManager;
+import android.media.MediaFormat;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Surface;
 import android.view.SurfaceControl;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 
-public class AndroidVideoView extends Surface implements SurfaceHolder.Callback {
-    public AndroidVideoView(@NonNull SurfaceControl from) {
-        super(from);
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class AndroidVideoView extends SurfaceView implements SurfaceHolder.Callback {
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder surfaceHolder;
+    private Uri mUri;
+    private Map<String, String> mHeaders;
+    private int mSeekWhenPrepared; //播放的位置  进度条的位置
+
+    public AndroidVideoView(Context context) {
+        super(context);
+        init();
     }
 
-    public AndroidVideoView(SurfaceTexture surfaceTexture) {
-        super(surfaceTexture);
+    public AndroidVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
     }
 
+    public AndroidVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setFixedSize(320, 220);   //显示的分辨率,不设置为视频默认
+    }
+
+    /**
+     * set uri
+     * @param uri
+     */
+    public void setVideoURI(Uri uri) {
+        setVideoURI(uri, null);
+    }
+
+    public void setVideoURI(Uri uri, Map<String, String> headers) {
+        mUri = uri;
+        mHeaders = headers;
+        mSeekWhenPrepared = 0;
+        openVideo();
+        requestLayout();
+        invalidate();
+    }
+
+    private void openVideo() {
+        if (mUri == null || surfaceHolder == null) {
+            // not ready for playback just yet, will try again later
+            return;
+        }
+        // we shouldn't clear the target state, because somebody might have
+        // called start() previously
+//        release(false);
+
+        if (mAudioFocusType != AudioManager.AUDIOFOCUS_NONE) {
+            // TODO this should have a focus listener
+            mAudioManager.requestAudioFocus(null, mAudioAttributes, mAudioFocusType, 0 /*flags*/);
+        }
+
+        try {
+            mMediaPlayer = new MediaPlayer();
+            // TODO: create SubtitleController in MediaPlayer, but we need
+            // a context for the subtitle renderers
+            final Context context = getContext();
+            final SubtitleController controller = new SubtitleController(
+                    context, mMediaPlayer.getMediaTimeProvider(), mMediaPlayer);
+            controller.registerRenderer(new WebVttRenderer(context));
+            controller.registerRenderer(new TtmlRenderer(context));
+            controller.registerRenderer(new Cea708CaptionRenderer(context));
+            controller.registerRenderer(new ClosedCaptionRenderer(context));
+            mMediaPlayer.setSubtitleAnchor(controller, this);
+
+            if (mAudioSession != 0) {
+                mMediaPlayer.setAudioSessionId(mAudioSession);
+            } else {
+                mAudioSession = mMediaPlayer.getAudioSessionId();
+            }
+            mMediaPlayer.setOnPreparedListener(mPreparedListener);
+            mMediaPlayer.setOnVideoSizeChangedListener(mSizeChangedListener);
+            mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            mMediaPlayer.setOnErrorListener(mErrorListener);
+            mMediaPlayer.setOnInfoListener(mInfoListener);
+            mMediaPlayer.setOnBufferingUpdateListener(mBufferingUpdateListener);
+            mCurrentBufferPercentage = 0;
+            mMediaPlayer.setDataSource(mContext, mUri, mHeaders);
+            mMediaPlayer.setDisplay(mSurfaceHolder);
+            mMediaPlayer.setAudioAttributes(mAudioAttributes);
+            mMediaPlayer.setScreenOnWhilePlaying(true);
+            mMediaPlayer.prepareAsync();
+
+            for (Pair<InputStream, MediaFormat> pending: mPendingSubtitleTracks) {
+                try {
+                    mMediaPlayer.addSubtitleSource(pending.first, pending.second);
+                } catch (IllegalStateException e) {
+                    mInfoListener.onInfo(
+                            mMediaPlayer, MediaPlayer.MEDIA_INFO_UNSUPPORTED_SUBTITLE, 0);
+                }
+            }
+
+            // we don't set the target state here either, but preserve the
+            // target state that was there before.
+            mCurrentState = STATE_PREPARING;
+            attachMediaController();
+        } catch (IOException ex) {
+            Log.w(TAG, "Unable to open content: " + mUri, ex);
+            mCurrentState = STATE_ERROR;
+            mTargetState = STATE_ERROR;
+            mErrorListener.onError(mMediaPlayer, MediaPlayer.MEDIA_ERROR_UNKNOWN, 0);
+            return;
+        } catch (IllegalArgumentException ex) {
+            Log.w(TAG, "Unable to open content: " + mUri, ex);
+            mCurrentState = STATE_ERROR;
+            mTargetState = STATE_ERROR;
+            mErrorListener.onError(mMediaPlayer, MediaPlayer.MEDIA_ERROR_UNKNOWN, 0);
+            return;
+        } finally {
+            mPendingSubtitleTracks.clear();
+        }
+    }
+
+
+    private SurfaceHolder holder;
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDisplay(surfaceHolder);    //设置显示视频显示在SurfaceView上
     }
 
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
+        surfaceHolder =
     }
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        mediaPlayer.release();
     }
 }
